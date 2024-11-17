dac3d1ffc1494cbd15546a03832b6f4fa447e8d2
Suganth46 <suganthsuganth749@gmail.com>
2024-11-17 11:16:09 +0530

    run the app

--- new template/javascript.js
+++ new template/javascript.js
@@ -1,13 +1,11 @@
 document.querySelector('.button').addEventListener('click', function() {
     alert('Starting the vulnerability scan...');
   });
-// Select elements
-
-document.getElementsByClassName("button").addEventListener("click",function(){
-  document.querySelector(".popup").style.display="flex";
-})
-
-document.querySelector(".close").addEventListener("click",function(){
-  document.querySelector(".popup").style.display="none";
-
-})
+  document.querySelector(".button").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "flex";
+  });
+  
+  document.querySelector(".close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+  });
+  
